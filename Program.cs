3fbeabae72091d307fc85c80c94dcf725216052f
Robert Muehler <robertmuehler@gmail.com>
2021-05-11 10:20:25 -0400

    added the parse, prompt for age

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace TicketsPrices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint age;
+            bool isStudent;
+
+            Console.WriteLine("Enter age: ");
+            try
+            {
+                age = UInt32.Parse(Console.ReadLine());
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Enter a correct age.");
+            }
         }
     }
 }
