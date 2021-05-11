abeddcb4dfd2b91fc9f62c50d82076db8429bcfb
Robert Muehler <robertmuehler@gmail.com>
2021-05-11 10:58:26 -0400

    finalized, added for loops

--- Program.cs
+++ Program.cs
@@ -9,37 +9,45 @@ namespace TicketsPrices
             uint age = default(uint);
             bool isStudent = default(bool);
 
-            Console.WriteLine("Enter age: ");
-            try
+            for (int i = 0; i < 100; i++)
             {
-                age = UInt32.Parse(Console.ReadLine());
-            }
-            catch (System.OverflowException e)
-            {
-                Console.WriteLine($"Enter a positive age.");
-            }
-            catch (System.FormatException e)
-            {
-                Console.WriteLine($"Enter a number.");
+                Console.WriteLine("Enter age: ");
+                try
+                 {
+                     age = UInt32.Parse(Console.ReadLine());
+                     break;
+                 }
+                 catch (System.OverflowException)
+                 {
+                     Console.WriteLine($"Enter a positive age.");
+                 }
+                 catch (System.FormatException)
+                 {
+                     Console.WriteLine($"Enter a number.");
+                 }
             }
 
-            Console.WriteLine("Are you a student? [y/n]");
-            var studentString = Console.ReadLine();
-            if(studentString.Equals("y",StringComparison.OrdinalIgnoreCase)){
-                isStudent = true;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Are you a student? [y/n]");
+                var studentString = Console.ReadLine();
+                if(studentString.Equals("y",StringComparison.OrdinalIgnoreCase)){
+                    isStudent = true;
+                    break;
+                }
+                else if(studentString.Equals("n",StringComparison.OrdinalIgnoreCase)){
+                    isStudent = false;
+                    break;
+                }
+                else{
+                    Console.WriteLine("Please enter valid input.");
+                }
             }
-            else if(studentString.Equals("n",StringComparison.OrdinalIgnoreCase)){
-                isStudent = false;
-            }
-            else{
-                Console.WriteLine("Please enter valid input.");
-                
-            }
-
+            
             if(age < 13 || isStudent) {
                 Console.WriteLine("Your ticket costs $8");
             }
-            else if(age >= 13 && age < 65) {
+            else if(age < 65) {
                 Console.WriteLine("Your ticket costs $14");
             }
             else {
